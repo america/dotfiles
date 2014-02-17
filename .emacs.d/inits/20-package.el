210e23445f3d074270770118d9b508f9e3d0ac9d
america <dreamers.ball66@gmail.com>
2014-02-18 02:02:57 +0900

    add init-loader.el

---
+++ .emacs.d/inits/20-package.el
@@ -0,0 +1,6 @@
+(require 'package)
+(add-to-list 'package-archives '("melpa" ."http://melpa.milkbox.net/packages/"))
+(when (< emacs-major-version 24)
+  (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/")))
+(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
+(package-initialize)
