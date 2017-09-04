ef1c458d4dcfe8da46c50b7d3f23cbec98462794
america <dreamers.ball66@gmail.com>
2017-09-04 18:31:58 +0900

    add .Xresources

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -62,16 +62,26 @@
         ("\\.sty$" . yatex-mode)) auto-mode-alist))
 ;;; set YaTeX coding system
 (setq YaTeX-kanji-code 4) ; UTF-8 の設定
+
 (add-hook 'yatex-mode-hook
-      '(lambda ()
+    '(lambda ()
          (setq YaTeX-use-AMS-LaTeX t) ; align で数式モードになる
          (setq YaTeX-use-hilit19 nil
            YateX-use-font-lock t)
-         (setq tex-command "uplatex") ; typeset command
+        
+	 (setq tex-command "uplatex") ; typeset command
+	 ; (local-set-key (kbd "C-c C-c"))
          (setq dvi2-command "evince") ; preview command
-         (setq tex-pdfview-command "xdg-open"))) ; preview command
+         (setq tex-pdfview-command "evince") ; preview command
+    ))
+   
+; dvi2-commandで自動的に拡張子を補完してくれるようにする設定
+(setq YaTeX-dvi2-command-ext-alist
+  '(("xdvi\\|dvipdfmx" . ".dvi")
+    ("ghostview\\|gv" . ".ps")
+    ("evince" . ".pdf")))
 
 ;;;
-;;; set config for magit
+;;; set configuration for magit
 ;;;
 (global-set-key (kbd "C-x g") 'magit-status)
