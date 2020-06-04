bb3fdc5de5b42c607abc97f63c869eb6fe1c5b0f
Administrator <dreamers.ball66@gmail.com>
2020-06-04 22:14:28 +0900

    更新

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -7,9 +7,12 @@
              "~/.emacs.d/auto-install"
              "~/.emacs.d/custom"
              "~/.emacs.d/conf"
-	     "~/.emacs.d/elisp")
+	     "~/.emacs.d/elisp"
+	     "~/.emacs.d/site-lisp")
            load-path))
 
+(add-to-list 'load-path "~/.emacs.d/site-lisp/elpa-mirror")
+(require 'elpa-mirror)
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; packages.el
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -34,20 +37,38 @@
     rjsx-mode
     company
     rust-mode
+    use-package
     )
   "packages to be installed")
 
+(setq gnutls-algorithm-priority "NORMAL:-VERS-TLS1.3")
+
 (require 'package)
 ;(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
-(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") t)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
 (add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/") t)
 (add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
 (add-to-list 'package-archives '("gnu", "http://elpa.gnu.org/packages/") t)
+;(add-to-list 'package-archives '("gnu", "https://mirrors.163.com/elpa/gnu/") t) 
+;(add-to-list 'package-archives '("gnu", "https://github.com/Malabarba/") t) 
+(add-to-list 'package-archives '("ELPA" . "http://tromey.com/elpa/") t)
 (package-initialize)
 (unless package-archive-contents (package-refresh-contents))
 (dolist (pkg my-favorite-package-list)
   (unless (package-installed-p pkg)
     (package-install pkg)))
+
+;;
+;; Auto Complete
+;;
+;; auto-complete-configの設定ファイルを読み込む。
+(require 'auto-complete-config)
+
+;; よくわからない
+(ac-set-trigger-key "TAB");
+
+;; auto-complete-modeを起動時に有効にする
+(global-auto-complete-mode t)
 
 ;; ~/.emacs.d/site-lisp 以下全部読み込み
 (let ((default-directory (expand-file-name "~/.emacs.d/site-lisp")))
@@ -65,7 +86,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (yatex yapfify rope-read-mode elpy powerline lsp-python company-lsp lsp-ui lsp-mode flycheck-rust racer rust-mode company el-get rjsx-mode json-mode flycheck js2-mode use-package package-utils rainbow-delimiters ac-php init-loader auto-complete howm elscreen flymake flymake-cursor ghc haskell-mode color-theme-solarized))))
+    (use-package-el-get magit undohist yatex yapfify rope-read-mode elpy powerline lsp-python company-lsp lsp-ui lsp-mode flycheck-rust racer rust-mode company el-get rjsx-mode json-mode flycheck js2-mode use-package package-utils rainbow-delimiters ac-php init-loader auto-complete howm elscreen flymake flymake-cursor ghc haskell-mode color-theme-solarized))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
