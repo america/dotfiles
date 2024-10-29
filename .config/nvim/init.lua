d471c3239bb0446da3fe644de3d9d927388e030c
america <dreamers.ball66@gmail.com>
2024-10-30 00:53:02 +0900

    Migrate Neovim configuration to init.lua

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,66 +1,69 @@
--- Ward off unexpected things that your distro might have made, as
--- well as sanely reset options when re-sourcing .vimrc
+-- 基本設定
+vim.opt.number = true                   -- 行番号を表示
+vim.opt.relativenumber = true           -- 相対行番号を表示
+vim.opt.expandtab = true                 -- タブをスペースに変換
+vim.opt.shiftwidth = 4                   -- 自動インデントの幅
+vim.opt.tabstop = 4                      -- タブ幅を4に設定
 
--- for xclip
+-- 自動バックアップ設定
+vim.opt.backup = true                    -- バックアップを有効にする
+vim.opt.backupdir = '~/.config/nvim/backup//'  -- バックアップファイルの保存先
+vim.opt.undofile = true                   -- Undoファイルを保存する
+vim.opt.undodir = '~/.config/nvim/undo//'      -- Undoファイルの保存先
+
+-- プラグインマネージャー設定
+vim.cmd [[packadd packer.nvim]]
+require('packer').startup(function(use)
+  use 'wbthomason/packer.nvim'          -- Packer自体の管理
+
+  -- プラグインの設定
+  use 'neovim/nvim-lspconfig'           -- LSP設定
+  use 'hrsh7th/nvim-cmp'                -- オートコンプリート
+  use 'hrsh7th/cmp-nvim-lsp'            -- LSPとの連携
+  use 'hrsh7th/cmp-buffer'              -- バッファ内の単語補完
+  use 'hrsh7th/cmp-path'                -- パス補完
+  use 'hrsh7th/cmp-vsnip'               -- スニペット補完
+  use 'hrsh7th/vim-vsnip'               -- スニペットエンジン
+  use 'kyazdani42/nvim-web-devicons'    -- アイコンサポート
+  use 'nvim-treesitter/nvim-treesitter' -- シンタックスハイライト
+  use 'tpope/vim-fugitive'              -- Git管理
+  use 'nvim-lualine/lualine.nvim'       -- ステータスライン
+  use {'akinsho/bufferline.nvim', requires = 'nvim-tree/nvim-web-devicons'} -- タブライン
+  use 'plasticboy/vim-markdown'         -- Markdown syntax highlighting
+  use 'godlygeek/tabular'                -- テーブルフォーマット支援
+end)
+
+-- プラグインの設定ファイルを読み込む
+require('cmp-config')                   -- オートコンプリート設定
+require('lsp-config')                   -- LSP設定
+require('lualine-config')               -- ステータスライン設定
+require('bufferline-config')             -- タブライン設定
+
+-- ショートカットキー設定
+vim.api.nvim_set_keymap('n', '<Tab>', ':BufferLineCycleNext<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<S-Tab>', ':BufferLineCyclePrev<CR>', { noremap = true, silent = true })
+
+-- xclipの設定
 vim.opt.clipboard:append('unnamedplus')
--- Loading external configuration file
+
+-- 外部設定ファイルの読み込み
 vim.cmd('runtime! option.vim')
 vim.cmd('runtime! keymap.vim')
 
--- Loading coc-settings.vim
+-- coc-settingsの読み込み
 if vim.fn.has('nvim') == 1 then
-  vim.cmd('source ~/.config/nvim/autoload/coc-settings.vim')
-  vim.cmd('source ~/.config/nvim/autoload/closed-tabs-managing.vim')
+    vim.cmd('source ~/.config/nvim/autoload/coc-settings.vim')
+    vim.cmd('source ~/.config/nvim/autoload/closed-tabs-managing.vim')
 else
-  vim.cmd('source ~/.vim/autoload/coc-settings.vim')
-  vim.cmd('source ~/.vim/autoload/closed-tabs-managing.vim')
+    vim.cmd('source ~/.vim/autoload/coc-settings.vim')
+    vim.cmd('source ~/.vim/autoload/closed-tabs-managing.vim')
 end
 
--- Attempt to determine the type of a file based on its name and possibly its
--- contents. Use this to allow intelligent auto-indenting for each filetype,
--- and for plugins that are filetype specific.
+-- ファイルタイプ設定
 if vim.fn.has('filetype') == 1 then
-  vim.cmd('filetype indent plugin on')
+    vim.cmd('filetype indent plugin on')
 end
 
 if vim.fn.has('syntax') == 1 then
-  vim.cmd('syntax on')
+    vim.cmd('syntax on')
 end
-
--- Packerの設定
-vim.cmd [[packadd packer.nvim]]
-
-require('packer').startup(function(use)
-  -- Packer自体の管理
-  use 'wbthomason/packer.nvim'
-
-  -- プラグインの例: LSP設定、オートコンプリート、シンタックスハイライト
-  use 'neovim/nvim-lspconfig'      -- LSP設定
-  use 'hrsh7th/nvim-cmp'           -- オートコンプリート
-  use 'hrsh7th/cmp-nvim-lsp'       -- LSPとの連携
-  use 'hrsh7th/cmp-buffer'         -- バッファ内の単語補完
-  use 'hrsh7th/cmp-path'           -- パス補完
-  use 'hrsh7th/cmp-vsnip'          -- スニペット補完（vsnipを使う場合）
-  use 'hrsh7th/vim-vsnip'          -- スニペットエンジン
-  use 'kyazdani42/nvim-web-devicons'
-
-  use 'nvim-treesitter/nvim-treesitter' -- シンタックスハイライト
-  use 'tpope/vim-fugitive'            -- Git管理
-  use 'nvim-lualine/lualine.nvim'     -- ステータスライン
-  use {'akinsho/bufferline.nvim', requires = 'nvim-tree/nvim-web-devicons'}
-  use 'plasticboy/vim-markdown'    -- Markdown syntax highlighting
-  use 'godlygeek/tabular'          -- Helps with table formatting
-
-end)
-
--- プラグインの設定ファイルを読み込み
-require('cmp-config')
-require('lsp-config')
-require('lualine-config')
-require('bufferline-config')
-
--- 自動バックアップ設定
-vim.opt.backup = true                -- バックアップを有効にする
-vim.opt.backupdir = '~/.config/nvim/backup//'  -- バックアップファイルの保存先
-vim.opt.undofile = true               -- Undoファイルを保存する
-vim.opt.undodir = '~/.config/nvim/undo//'      -- Undoファイルの保存先
